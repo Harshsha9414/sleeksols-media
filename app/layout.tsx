c65d3e57596e0001d87b4c92fc16ea02b5c3f10f
Harsh Sharma <harsh941445@gmail.com>
2025-05-30 18:08:08 +0530

    fix gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id=GTM-P7NQWH29'+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P7NQWH29');
+            })(window,document,'script','dataLayer');
           `}
         </Script>
       </head>
