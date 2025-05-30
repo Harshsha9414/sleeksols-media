54584257594e55bf7af2d4818214328114893886
Harsh Sharma <harsh941445@gmail.com>
2025-05-30 17:43:26 +0530

    Fix gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,16 +34,18 @@ export default function RootLayout({
       <head>
         {/* Google Tag Manager */}
         <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-P7NQWH29');`,
-          }}
-        />
+  id="gtm-head"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id=GTM-P7NQWH29'+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-P7NQWH29');
+    `,
+  }}
+/>
       </head>
       <body className="font-sans antialiased">
       <noscript>
