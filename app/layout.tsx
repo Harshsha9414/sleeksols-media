0eceae4a14f5ccefb9451cb1071e5b975aceb15d
Harsh Sharma <harsh941445@gmail.com>
2025-05-30 18:02:28 +0530

    fix gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,32 +31,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${spaceGrotesk.variable} ${inter.variable}`}>
-      <head>
-        {/* Google Tag Manager */}
-        <Script
-  id="gtm-head"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id=GTM-P7NQWH29'+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-P7NQWH29');
-    `,
-  }}
-/>
+       <head>
+        {/* GTM Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P7NQWH29');
+          `}
+        </Script>
       </head>
       <body className="font-sans antialiased">
+      {/* GTM noscript fallback */}
       <noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-P7NQWH29"
-    height="0"
-    width="0"
-    style={{ display: 'none', visibility: 'hidden' }}
-  ></iframe>
-</noscript>
-
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7NQWH29"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
