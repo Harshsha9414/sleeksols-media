8221cd92369775c5701f51bfe1aad72e366af6b5
Harsh Sharma <harsh941445@gmail.com>
2025-05-30 17:38:14 +0530

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 import type { Metadata } from 'next';
 import { Space_Grotesk, Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
@@ -30,7 +31,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${spaceGrotesk.variable} ${inter.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P7NQWH29');`,
+          }}
+        />
+      </head>
       <body className="font-sans antialiased">
+      <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-P7NQWH29"
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+  ></iframe>
+</noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
